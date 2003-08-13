1f1a78c730c6c8b170c0c8043f70b02293fcf2cc
Alex Bowley <kilinrax@cpan.org>
2003-08-13 14:36:48 +0000

    Import of KILINRAX/Array-Dissect-1.01 from CPAN.
    gitpan-cpan-distribution: Array-Dissect
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         KILINRAX/Array-Dissect-1.01.tar.gz
    gitpan-cpan-author:       KILINRAX
    gitpan-cpan-maturity:     released

--- Dissect.pm
+++ Dissect.pm
@@ -3,36 +3,80 @@ package Array::Dissect;
 use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-use Carp qw( croak );
+use Carp;
 
 require Exporter;
 
-@ISA = qw(Exporter AutoLoader);
+our @ISA = qw(Exporter DynaLoader);
+
 # Items to export into callers namespace by default. Note: do not export
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
-@EXPORT_OK = qw(
-	reform
-);
 
-$VERSION = '1.00';
+# This allows declaration	use Array::Dissect ':all';
+# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
+# will save memory.
+our %EXPORT_TAGS = ( 'all' => [ qw(
+                                   reform
+                                   dissect
+                                  ) ] );
+
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+
+our @EXPORT = qw();
+
+our $VERSION = '1.01';
 
 # Preloaded methods go here.
+sub reform {
+  my ($size, $r_list) = &_validate_params;
 
-sub dissect ($$\@) {
-  my $class = shift;
-  my $size = shift;
-  croak "Invalid array size" if $size < 1;
+  my @list = @{$r_list};
+  my @lol;
 
-  my @bigarray = @{ shift() };
-  my @smallarrays;
-  my $i = 0;
-  my $j = 0;
-  foreach (@bigarray) {
-    $smallarrays[$i]->[$j] = $_;
+  push @lol, [splice @list, 0, $size] while @list;
+
+  return wantarray ? @lol : \@lol;
+}
+
+sub dissect {
+  my ($size, $r_list) = &_validate_params;
+
+  my @lol;
+  my ($i, $j) = (0, 0);
+
+  foreach (@$r_list) {
+    $lol[$i]->[$j] = $_;
     $i = 0, $j++ unless (++$i % $size);
   }
-  return @smallarrays;
+
+  return wantarray ? @lol : \@lol;
+}
+
+
+# Internal parameter validation function
+sub _validate_params {
+  # Check we have at been called with at least 2 arguments
+  Carp::croak( "Called with insufficient arguments" ) if( $#_ < 1 );
+
+  # First argument is size. check it is a valid positive integer.
+  my $size = shift;
+  if( $size !~ /^+?\d+$/ ) {
+    Carp::croak( "Size '$size' is not a valid positive integer" );
+  } elsif( $size == 0 ) {
+    Carp::croak( "'$size' is an invalid array size" );
+  }
+
+  # If only one argument remains, check to see if it is an arrayref, otherwise, reate a reference to all remaining args
+  my $r_list;
+  if( ($#_ == 0) &&
+      (ref($_[0]) eq 'ARRAY') ) {
+    $r_list = $_[0];
+  } else {
+    $r_list = \@_;
+  }
+
+  return $size, $r_list;
 }
 
 # Autoload methods go after =cut, and are processed by the autosplit program.
@@ -43,16 +87,25 @@ __END__
 
 =head1 NAME
 
-Array::Dissect - Convert an array into N-sized array of arrays
+Array::Dissect - Convert an array into N-sized array of arrays.
 
 =head1 SYNOPSIS
 
-  use Array::Dissect;
+  use Array::Dissect qw( :all );
 
   @sample = ( 1 .. 10 );
   $rowsize = 3;
 
-  Array::Dissect->dissect( $rowsize, \@sample );
+  reform( $rowsize, @sample );
+      =>
+         (
+            [   1,   2,   3   ],
+            [   4,   5,   6   ],
+            [   7,   8,   9   ],
+            [   10   ]
+          );
+
+  dissect( $rowsize, @sample );
       =>
          (
             [   1,   5,   9   ],
@@ -64,10 +117,33 @@ Array::Dissect - Convert an array into N-sized array of arrays
 
 =head1 DESCRIPTION
 
-Like Array::Reform, this takes a big array, and converts it into an array of arrays. The key difference between the two modules is that this one takes elements from the start of the big array and pushes them onto each of the subarrays sequentially, rather than simply dividing the big array up into discrete chunks. The intended effect of this method is that, if fed a sorted array, dissect() returns an array of arrays where the first element of each subarray will be one of the highest (or lowest, depending on how the array is sorted) value items in the big array, and the last element of each will be the opposite.
+This is intended as a much-improved replacement for Array::Reform.
+Given the current maintainer's apparent lack of interest in keeping
+this module up to date, it unfortunately, for now, exists separately.
+
+Both these methods are designed to reformat a list into a list of
+lists. It is often used for formatting data into HTML tables, amongst
+other things.
+
+The key difference between the two methods is that C<dissect()> takes
+elements from the start of the list provided and pushes them onto each
+of the sub-arrays sequentially, rather than simply dividing the list
+into discrete chunks.
+As a result C<dissect()> returns a list of lists where the first
+element of each sublist will be one of the first elements of the
+source list, and the last element will be one of the last.
+This behavior is much more useful when the input list is sorted.
+
+With both methods the array to be reformed can be passed as an array
+or an array reference.
 
 =head1 AUTHOR
 
-Alex Bowley <kilinrax@cpan.org>
+Alex Bowley <kilinrax@cpan.org>, implementing improvements to
+Array::Reform suggested by Dave Cross <dave@dave.org.uk>.
+
+=head1 SEE ALSO
+
+Array::Reform.
 
 =cut
--- test.pl
+++ test.pl
@@ -1,26 +1,47 @@
 # Before `make install' is performed this script should be runnable with
 # `make test'. After `make install' it should work as `perl test.pl'
 
-BEGIN { $| = 1; print "1..3\n"; }
-END {print "not ok 1\n" unless $loaded;}
-use Array::Dissect;
-$loaded = 1;
-print "ok 1\n";
+#########################
+
+# change 'tests => 1' to 'tests => last_test_to_print';
+
+use Test;
+BEGIN { plan tests => 8 };
+use Array::Dissect qw( :all );
+ok(1); # If we made it this far, we're ok.
+
+#########################
+
+# Insert your test code below, the Test module is use()ed here so read
+# its man page ( perldoc Test ) for help writing this test script.
+
+use vars qw( @orig $size @new $new );
 
 @orig = ( 1 .. 16 );
+$size = 8;
+@new = reform( $size, \@orig );
+ok( scalar @new == 2 );
+
+# check calling with an array rather than an arrayref works
+@new = reform( $size, @orig );
+ok( scalar @new == 2 );
+
+$size = 4;
+$new = reform( $size, \@orig );
+ok( scalar @$new == 4 );
+
 $size = 5;
-@new = Array::Dissect->dissect( $size, \@orig );
-if ( scalar @new == 5 ) {
-  print "ok 2\n";
-} else {
-  print "not ok 2\n";
-}
+@new = dissect( $size, \@orig );
+ok( scalar @new == 5 );
 
 @orig = ( 1 .. 5 );
+$size = 2;
+@new = reform( $size, \@orig );
+ok( $new[0][1] == 2 && $new[2][0] == 5 );
+
+@new = dissect( $size, \@orig );
+ok( $new[1][0] == 2 && $new[0][1] == 3 );
+
 $size = 3;
-@new = Array::Dissect->dissect( $size, \@orig );
-if ( $new[0][1] == 4 && $new[2][0] == 3 ) {
-  print "ok 3\n";
-} else {
-  print "not ok 3\n";
-}
+@new = dissect( $size, \@orig );
+ok( $new[0][1] == 4 && $new[2][0] == 3 );
